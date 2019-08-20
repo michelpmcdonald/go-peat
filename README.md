e038c43cd0486063695d9462d867e0820c66480e
Michel McDonald <michelpmcdonald@gmail.com>
2019-08-20 18:04:18 -0400

    Added pause, unit test, API methods

--- README.md
+++ README.md
@@ -2,6 +2,49 @@
 
 ## Overview [![GoDoc](https://godoc.org/github.com/michelpmcdonald/go-peat?status.svg)](https://godoc.org/github.com/michelpmcdonald/go-peat) [![Build Status](https://travis-ci.org/michelpmcdonald/go-peat.svg?branch=master)](https://travis-ci.org/michelpmcdonald/go-peat) [![Go Report Card](https://goreportcard.com/badge/github.com/michelpmcdonald/go-peat)](https://goreportcard.com/report/github.com/michelpmcdonald/go-peat)
 
+## Motivation and Example Uses
+
+I've needed common replay capability in several projects. Gopeat(re-peat idk...) was created as a general replay library(DRY) that can be used with a time-stamped sorted data stream that can be represented as a golang struct with a time.Time getter.
+
+Some Examples:
+
+Equities Market: historic tick data chart replay for trader training
+
+Live data stream proxy for development-testing of soft real-time trading systems, live dash boards and live GIS gps tracking data feeds.
+
+Distributed Interactive Simulation(DIS): After Action Review(AAR) GIS overlay replay of DIS packets recorded during training and experimentation with Soldiers using new equipment and tactics.
+
+
+
+## Tech details
+
+Internally gopeat runs two goroutines, a buffered read-ahead data stream fetcher and a sender that invokes a client's callback with the client's data at the appropriate replay time.
+
+The callback model seemed to offer the most flexibility without exposing channels and WaitGroups(Avoid concurrency in your API - [Go Best Practices](https://talks.golang.org/2013/bestpractices.slide#25)
+)
+
+A back-pressure adjustment internally monitors the amount of time the client callback spends before returning and dynamically makes timing adjustments.  For example, if callbacks are taking 15 milliseconds callbacks will be executed 15 milliseconds earlier that the exact replay time derived from the time-stamped data in an attempt to "lead" the client.
+
+The client should return from the callback as fast as possible to 
+maximize replay time accuracy.
+
+## Usage Notes
+In order to utilize, the client should:
+
+*Create a time stamp source struct that implements the gopeat.TimeStamper(name inspired by golang [fmt.Stringer](https://golang.org/pkg/fmt/#Stringer))
+
+*Create a time stamper data source that implements gopeat.TimeBracket interface(sets replay start and end time) and implements gopeat.TimeStampSource.Next() iterator like interface that provides
+the "TimeStamper" structs from the above step.
+gopeat.CsvTsSource is a provided stamper data source for data stored
+in Csv format. Use that directly or view the source to get an idea of how to implement your own source.
+
+*Create a callback func that matches the gopeat.OnTsDataReady func type.
+
+3 steps(2.5 if gopeat.CsvTsSource is used) to get going
+
+
+
+
 ## Install
 
 ```
@@ -38,14 +81,15 @@ func (ts tsRec) GetTimeStamp() time.Time {
 // Now some data is needed, typically this would
 // be from a csv file, but whatever suits ya
 var tsCsv = `weird_name, amt
-	     09/01/2013 17:00:00.083 UTC, 12
-	     09/01/2013 17:00:00.088 UTC, 55
-	     09/01/2013 17:00:00.503 UTC, 54
-	     09/01/2013 17:00:03.201 UTC, 54`
+		    09/01/2013 17:00:00.083 UTC, 12
+		    09/01/2013 17:00:00.088 UTC, 55
+			09/01/2013 17:00:00.503 UTC, 54
+			09/01/2013 17:00:03.201 UTC, 54`
 
 // Define func to convert a csv line slice to our struct
 func csvToTsRec(csv []string) (gopeat.TimeStamper, error) {
-	tim, _ := time.Parse("01/02/2006 15:04:05.999 MST", strings.TrimSpace(csv[0]))
+	tim, _ := time.Parse("01/02/2006 15:04:05.999 MST",
+		strings.TrimSpace(csv[0]))
 	amt, _ := strconv.ParseFloat(strings.TrimSpace(csv[1]), 64)
 	return tsRec{tsWeirdName: tim, amt: amt}, nil
 }
@@ -53,8 +97,8 @@ func csvToTsRec(csv []string) (gopeat.TimeStamper, error) {
 func main() {
 
 	// A data source is needed. Since our demo data is csv, use
-	// gopeats csv time stamper source and provide the data and 
-	// the csv line converter. It implements the needed interfaces. 
+	// gopeats csv time stamper source and provide the data and
+	// the csv line converter. It implements the needed interfaces.
 	// Create your own source as needed, just implement
 	// TimeBracket and TimeStampSource interfaces
 	tsSource := &gopeat.CsvTsSource{
@@ -63,9 +107,9 @@ func main() {
 		CsvTsConv: csvToTsRec,
 	}
 
-	// Playback controls the replay run
+	// PlayBack controls the sim run
 	var sim *gopeat.PlayBack
-	
+
 	// Create a callback to handle data, called when the simulation time
 	// reaches the data's timestamp, so it's in sim soft real time
 	recCnt := 0
@@ -73,12 +117,12 @@ func main() {
 		recCnt++
 		if recCnt > 2 {
 			// Stop the sim early, skip last record
-			close(sim.StopChan)
+			sim.Quit()
 		}
 		tsd := ts.(tsRec)
-		fmt.Printf("Data Time: %v. Data Amt: %f\n", 
-				   tsd.GetTimeStamp(),
-				   tsd.amt)
+		fmt.Printf("Data Time: %v. Data Amt: %f\n",
+			tsd.GetTimeStamp(),
+			tsd.amt)
 		return nil
 	}
 
@@ -91,9 +135,13 @@ func main() {
 		2,       //Sim rate
 		dataOut) //Call back
 
-	//Play it blocks til done
+	// Start the replay
 	sim.Play()
+
+	// Block until done
+	sim.Wait()
 }
+
 
 ```
 
--- gopeat_test.go
+++ gopeat_test.go
@@ -1,7 +1,6 @@
 package gopeat
 
 import (
-	"fmt"
 	"sync"
 	"testing"
 	"time"
@@ -44,30 +43,64 @@ func (st *mockTsDataSource) SetStartTime(startTime time.Time) {
 func (st *mockTsDataSource) SetEndTime(endTime time.Time) {
 }
 
-// TestPlay confirms that a value sent into playback is sent within
+// A datasource that blocks on next, client should release Wg so next
+// does not block the load data goroutine from shutting down
+type mockTsBlockingDs struct {
+	Wg         sync.WaitGroup
+	NextCalled bool
+}
+
+func (st *mockTsBlockingDs) Next() (TimeStamper, bool) {
+	st.Wg.Add(1)
+	st.NextCalled = true
+	st.Wg.Wait()
+	return nil, false
+}
+
+func (st *mockTsBlockingDs) SetStartTime(startTime time.Time) {
+}
+
+func (st *mockTsBlockingDs) SetEndTime(endTime time.Time) {
+}
+
+// TestSendSpeed confirms that a value sent into playback is sent within
 // 3 milliseconds of the proper time
-func TestPlay(t *testing.T) {
+func TestSendSpeed(t *testing.T) {
+	// Create a new PlayBack at 2x rate
 	var mts mockTsDataSource
-	var wg sync.WaitGroup
-	wg.Add(1)
-	simStartTime := time.Now() //11:00:00
+	simStartTime := time.Now()
 	dataTime := simStartTime.Add(time.Second * 1)
 	pb, _ := New("test", simStartTime, dataTime, &mts, 2, nil)
+
+	// Callback measures time to first data playback send.  Since
+	// the first time stamper is 1 second after playback start,
+	// the first callback should be at .5 seconds given the rate is 2x
+	callbackHit := false
 	pb.SendTs = func(ts TimeStamper) error {
 		wallDur := time.Since(simStartTime)
+		callbackHit = true
 		if ts.(mockTsData).Val != 6 {
 			t.Errorf("Val = %d; want 6", ts.(mockTsData).Val)
 		}
-		if wallDur.Seconds() > .503 {
-			t.Errorf("Time = %f; want less than .505", wallDur.Seconds())
+		if wallDur.Seconds() >= .503 {
+			t.Errorf("Time = %f; want less than .503", wallDur.Seconds())
 		}
 		return nil
 	}
-	// Write the time stamped data buffer to the data chan
+	pb.init()
+	// Create a timestamper with a timestamp 1 second out after
+	// start time and push it into the playback data chan
 	pb.tsDataChan <- []TimeStamper{mockTsData{Tim: dataTime, Val: 6}}
 	close(pb.tsDataChan)
-	pb.send(&wg)
-	fmt.Println("done")
+
+	// run send, it will execute callback
+	pb.wg.Add(1)
+	pb.send()
+
+	// Make sure call was called
+	if !callbackHit {
+		t.Errorf("Provided PlayBack call was not executed")
+	}
 }
 
 // TestSimRate confirms that the user provided sim rate is translated
@@ -89,6 +122,7 @@ func TestLoadTsData(t *testing.T) {
 
 	// Create a new playback that uses mts
 	pb, _ := New("test", time.Now(), time.Now(), &mts, 2, nil)
+	pb.init()
 
 	// Set a read buffer size of 10
 	pb.tsDataBufSize = 10
@@ -113,5 +147,195 @@ func TestLoadTsData(t *testing.T) {
 	if readCnt != 3 {
 		t.Errorf("Got %d Chan reads; expected 3", readCnt)
 	}
+}
 
+func TestCreateNoSource(t *testing.T) {
+	_, err := New("test", time.Now(), time.Now(), nil, 2, nil)
+
+	if err == nil {
+		t.Errorf("Got Empty error, expected error")
+	}
+	expected := "playBack: tsSource required"
+	if err.Error() != "playBack: tsSource required" {
+		t.Errorf("Got %s error, expected %s", err.Error(), expected)
+	}
+}
+
+func TestPlay(t *testing.T) {
+	// Create a new mocked data source that emits 23 time stamper values
+	mts := mockTsBlockingDs{}
+
+	// Create a new playback that uses mts
+	pb, _ := New("test", time.Now(), time.Now(), &mts, 2, nil)
+
+	// Active flag should start as false
+	if pb.replayActive {
+		t.Errorf("replay active true, expected false")
+	}
+
+	pb.Play()
+
+	// play worked if it set active flag to true
+	if !pb.replayActive {
+		t.Errorf("replay active false, expected true")
+	}
+
+	// play should start data loading from TimeStamper Source, so
+	// confirm source's Next() was called
+	if !mts.NextCalled {
+		t.Error("NextCalled is false, expected true")
+	}
+
+	// Release mts so we don't leak loader goroutine
+	close(pb.quitChan)
+	mts.Wg.Done()
+
+	// Ok at this point, no data was loaded, dataloader should be
+	// closing, and sender should be closing and releasing PlayBacks
+	// Blocking wait
+	pb.Wait()
+
+	// Make sure loader closed dat chan to loader confirm exit-cleanup
+	select {
+	case _, ok := <-pb.tsDataChan:
+		// should be closed with no data
+		if ok {
+			t.Error("tsDataChan was not empty, expected it to be empty")
+		}
+	default:
+		t.Error("tsDataChan is still open, expected it to be closed")
+	}
+}
+
+func TestPause(t *testing.T) {
+	// Create a new mocked data source that emits 23 time stamper values
+	mts := mockTsBlockingDs{}
+
+	// Create a new playback that uses mts
+	pb, _ := New("test", time.Now(), time.Now(), &mts, 2, nil)
+
+	// Need a pause chan for Pause() to signal
+	pb.pauseChan = make(chan struct{})
+
+	// Create a closed resumeChan, this is
+	// a possible state after a pause-resume cycle
+	pb.resumeChan = make(chan struct{})
+	close(pb.resumeChan)
+	pb.replayActive = true
+	pb.paused = false
+
+	pb.Pause()
+
+	// Make sure pause closed pauseChan to signal a pause
+	select {
+	case _, ok := <-pb.pauseChan:
+		// should be closed with no data
+		if ok {
+			t.Error("pauseChan was not empty, expected it to be empty")
+		}
+	default:
+		t.Error("pauseChan is still open, expected it to be closed")
+	}
+
+	// Pause worked if it set active flag to true
+	if !pb.paused {
+		t.Error("pause flag is false, expected true")
+	}
+
+	select {
+	case _, ok := <-pb.resumeChan:
+		if !ok {
+			t.Error("resumeChan is closed, expected it to be open")
+		}
+	default:
+	}
+}
+
+func TestPauseAlreadyPaused(t *testing.T) {
+	// Create a new mocked data source that emits 23 time stamper values
+	mts := mockTsBlockingDs{}
+
+	// Create a new playback that uses mts
+	pb, _ := New("test", time.Now(), time.Now(), &mts, 2, nil)
+	pb.pauseChan = make(chan struct{})
+	pb.replayActive = false
+	pb.paused = true
+
+	pb.Pause()
+
+	// Since pb is already in the paused state,
+	// Pause() should do nothing
+	select {
+	case <-pb.pauseChan:
+		t.Error("pauseChan signaled, expected to be un-signaled")
+	default:
+	}
+
+	if pb.replayActive {
+		t.Error("replay active true, expected to be false")
+	}
+	if !pb.paused {
+		t.Error("paused false, expected to be true")
+	}
+}
+
+func TestResume(t *testing.T) {
+	// Create a new mocked data source that emits 23 time stamper values
+	mts := mockTsBlockingDs{}
+
+	// Create a new playback that uses mts
+	pb, _ := New("test", time.Now(), time.Now(), &mts, 2, nil)
+
+	// Need a resume chan for Resume() to signal
+	pb.resumeChan = make(chan struct{})
+
+	// Create a closed pauseChan, this is
+	// a possible state after a pause
+	pb.pauseChan = make(chan struct{})
+	close(pb.pauseChan)
+	pb.paused = true
+
+	pb.Resume()
+
+	// Make sure resume closed resumeChan to signal a pause
+	select {
+	case _, ok := <-pb.resumeChan:
+		// should be closed with no data
+		if ok {
+			t.Error("resumeChan was not empty, expected it to be empty")
+		}
+	default:
+		t.Error("resumeChan is still open, expected it to be closed")
+	}
+
+	// Pause worked if it set active flag to true
+	if pb.paused {
+		t.Error("pause flag is false, expected true")
+	}
+
+	select {
+	case _, ok := <-pb.pauseChan:
+		if !ok {
+			t.Error("pausehan is closed, expected it to be open")
+		}
+	default:
+	}
+}
+
+func TestQuit(t *testing.T) {
+	// Create a new mocked data source that emits 23 time stamper values
+	mts := mockTsBlockingDs{}
+
+	// Create a new playback that uses mts
+	pb, _ := New("test", time.Now(), time.Now(), &mts, 2, nil)
+	pb.replayActive = true
+	pb.quitChan = make(chan struct{})
+
+	pb.Quit()
+
+	select {
+	case <-pb.quitChan:
+	default:
+		t.Error("quitChan not signaled, expected it to be signaled")
+	}
 }
